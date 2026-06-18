8120cf538ddcb8427f8f42bba873a3554a30d742
Krutarth Patel <mail2krutarthpatel@gmail.com>
2026-06-18 12:38:46 -0400

    created turtle which moves forward, a screen, and a basic car

---
+++ car.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+import random
+
+class Car(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.shapesize(stretch_len=2, stretch_wid=1)
+        # self.color("blue")
+        self.speed("fastest")
+        x= random.randint(-280, 280)
+        y= random.randint(-280, 280)
+        self.goto(x, y)
---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+from turtle import Screen, Turtle
+from car import Car
+
+
+screen = Screen()
+# screen.tracer(0)
+screen.setup(width=600, height=600)
+screen.bgcolor("white")
+screen.title("Turtle Crossing Game")
+game_is_on = True
+
+turtle = Turtle("turtle")
+turtle.penup()
+turtle.color("black")
+turtle.setheading(90)
+turtle.goto(0, -270)
+# turtle.speed(10)
+car = Car()
+
+
+def MoveUp():
+    turtle.forward(10)
+
+screen.listen()
+screen.onkey(MoveUp, "Up")
+
+
+
+
+screen.exitonclick()
