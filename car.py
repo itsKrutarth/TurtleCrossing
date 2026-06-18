a8349b6797ea010b29031c74131164fc8de5cf99
Krutarth Patel <mail2krutarthpatel@gmail.com>
2026-06-18 16:09:15 -0400

    created cars moving

--- car.py
+++ car.py
@@ -1,19 +1,30 @@
 from turtle import Turtle
 import random
 
-class Car(Turtle):
+class Car():
 
-    def __init__(self, color):
-        super().__init__()
-        self.shape("square")
-        self.penup()
-        self.shapesize(stretch_len=2, stretch_wid=1)
-        self.color(color)
-        self.speed("fastest")
-        self.setheading(180)
-        x= 280
-        y= random.randint(-260, 280)
-        self.goto(x, y)
+    def __init__(self):
+        self.all_cars = []
+        
+
+    def createCar(self):
+        randomChance1 = 3
+        randomChance2 = 6
+        randNum = random.randint(0, 10)
+        if(randNum==randomChance1 or randNum==randomChance2):
+            color = ["blue", "black", "green", "yellow", "orange", "red", "grey", "purple"]
+            car = Turtle()
+            car.shape("square")
+            car.penup()
+            car.shapesize(stretch_len=2, stretch_wid=1)
+            car.color(color[random.randint(0,7)])
+            car.speed("fastest")
+            car.setheading(180)
+            x= 280
+            y= random.randint(-260, 260)
+            car.goto(x, y)
+            self.all_cars.append(car)
     
     def moveCar(self, rate):
-        self.forward(rate)
+        for car in self.all_cars:
+            car.forward(rate)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 
 
 screen = Screen()
-# screen.tracer(0)
+screen.tracer(0)
 screen.setup(width=600, height=600)
 screen.bgcolor("white")
 screen.title("Turtle Crossing Game")
@@ -18,20 +18,18 @@ turtle.color("black")
 turtle.setheading(90)
 turtle.goto(0, -270)
 # turtle.speed(10)
-# car = Car()
-color = ["blue", "black", "green", "yellow", "orange", "red", "grey", "purple"]
-while game_is_on:
-    color1 = color[random.randint(0,7)]
-    car = Car(color1)
-    car.moveCar(10)
-
-
+cars = Car()
 def MoveUp():
     turtle.forward(10)
 
 screen.listen()
 screen.onkey(MoveUp, "Up")
 
+while game_is_on:
+    time.sleep(0.1)
+    cars.createCar()
+    cars.moveCar(10)
+    screen.update()
 
 
 
